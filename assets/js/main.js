5b851e0184b010b50fa347ceb1098240b1527688
Marco Zellini <zellinimarco@gmail.com>
2023-08-25 14:07:50 +0200

    Add Loop and List Elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,27 @@
     Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
 */
 
+const bodyElement = document.querySelector('body');
+const ulElement = document.createElement('ul');
+
+bodyElement.append(ulElement);
+
+for (let i = 1; i <= 100; i++) {
+    
+    const liElement = document.createElement('li');
+    ulElement.append(liElement);
+
+    if ( (i % 3) === 0 && (i % 5) === 0 ) {
+        console.log("FizzBuzz");
+        liElement.append("FizzBuzz")
+    } else if ( (i % 3) === 0 ) {
+        console.log("Fizz");
+        liElement.append("Fizz")
+    } else if ( (i % 5) === 0 ) {
+        console.log("Buzz");
+        liElement.append("Buzz")
+    } else {
+        console.log(i);
+        liElement.append(i) 
+    }
+}
